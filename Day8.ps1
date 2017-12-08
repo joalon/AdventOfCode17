a86b1c640cfbaef291bdfb0cf87e1d64efc44272
Joakim Lönnegren <joakim.lonnegren@santanderconsumer.se>
2017-12-08 15:59:36 +0100

    starting day 8

---
+++ Day8.ps1
@@ -0,0 +1,25 @@
+﻿$file = Get-Content .\input_day8.txt
+
+Enum Operator {
+    INC
+    DEC
+}
+
+class KeyValuePair {
+    [string]$Key
+    [int]$Value
+}
+
+class Instruction {
+
+    [KeyValuePair]$register
+    [Operator]$Operator
+    [int]$Value
+
+
+    Instruction () {
+        
+    }
+
+}
+

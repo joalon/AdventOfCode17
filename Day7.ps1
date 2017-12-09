c8dfbeffb6e17e14ab8d8651e39c273c7edeba66
root <joakimlonnegren@gmail.com>
2017-12-09 16:50:00 +0100

    Solved day 9

--- Day7.ps1
+++ Day7.ps1
@@ -1,2 +1,140 @@
 ﻿$file = Get-Content .\input_day7.txt
 
+$test = "pbga (66)",
+"xhth (57)",
+"ebii (61)",
+"havc (66)",
+"ktlj (57)",
+"fwft (72) -> ktlj, cntj, xhth",
+"qoyq (66)",
+"padx (45) -> pbga, havc, qoyq",
+"tknk (41) -> ugml, padx, fwft",
+"jptl (61)",
+"ugml (68) -> gyxo, ebii, jptl",
+"gyxo (61)",
+"cntj (57)"
+
+class Program {
+    [string]$Name
+    [int]$Weight
+    [string[]]$ChildrensNames
+    [Program[]]$Children
+    [Program[]]$Parent
+
+    Program([string]$name, [int]$weight){
+        $this.Name = $name
+        $this.Weight = $weight
+        $this.ChildrensNames = @()
+        $this.Children = @()
+    }
+
+    Program([string]$name, [int]$weight, [string[]]$childrensNames) {
+        $this.Name =$name
+        $this.Weight = $weight
+        $this.ChildrensNames = $childrensNames
+        $this.Children = @()
+    }
+}
+
+
+Function ParseProgram {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$unparsedProgram
+    )
+
+    Process 
+    {
+        [string[]]$splitString = $unparsedProgram -split '\W\s*'
+
+        $splitString = $splitString | ?{$_} # Remove empty array positions
+
+        $programName = $splitString[0]
+        $programWeight = [int]$splitString[1]
+        $childrensNames = @()
+        if($splitString.Count -ge 3) {
+            
+            for($i = 2; $i -lt $splitString.Count; $i++) {
+                $childrensNames += $splitString[$i]
+            }
+        }
+
+        return [Program]::new($programName, $programWeight, $childrensNames)
+    }
+}
+
+
+[Program[]]$unsorted = @()
+#$file | foreach { $unsorted += (ParseProgram $_) }
+$file | foreach { $unsorted += (ParseProgram $_) }
+
+[Program[]]$nextPass = @()
+$unsorted | foreach { $nextPass += $_}
+
+foreach ($prog in $unsorted) {
+    if($prog.ChildrensNames -gt 0){
+        foreach($childName in $prog.ChildrensNames) {
+
+            $child = [System.Linq.Enumerable]::Single($unsorted, [Func[Program,bool]]{ param($p) $p.Name -eq $childName })
+            
+            $child.Parent = $prog
+            $prog.Children += $child
+        }
+    }
+}
+
+[Program]$root = [System.Linq.Enumerable]::Single($unsorted, [Func[Program,bool]]{ param($p) $p.Parent -eq $null })
+
+function CalculateChildWeights {
+
+    Param(
+        [Program]$node
+    )
+
+    Process {
+        if($node.Children.Count -eq 0) {
+            return $node.Weight
+        }
+        
+        [int]$childWeights = 0
+
+        foreach($child in $node.Children) {
+            $childWeights += (CalculateChildWeights $child)
+        }
+
+        return $childWeights
+    }
+}
+
+function CalculateLayerWeights {
+    Param(
+        [Program]$node,
+        [int]$targetLayer,
+        [int]$currentLayer = 0
+    )
+    process {
+        if($targetLayer -eq $currentLayer){
+            return $node.Weight
+        }
+
+        [int]$layerWeight = 0
+        foreach($child in $node.Children) {
+            $layerWeight += (CalculateLayerWeights $child $targetLayer ($currentLayer++))
+        }
+
+        return $layerWeight
+    }
+}
+
+write-host "Child weights: "
+foreach($child in $root.Children) {
+    $weight = (CalculateChildWeights $child)
+    write-host $child.Name ": " ($weight + $child.Weight)
+}
+
+write-host "layer weights: "
+foreach($child in $root.Children) {
+    $weight = (CalculateLayerWeights $child 10)
+    write-host $child.Name ": " $weight
+}
+
---
+++ Day9.ps1
@@ -0,0 +1,125 @@
+﻿$file = Get-Content .\input_day9.txt
+
+Function CleanGarbage {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$inputFile
+    )
+
+    Process {
+
+        $stringWithoutEscapedTags = $inputFile -replace "(!.)"
+
+        $nearlyClean = $stringWithoutEscapedTags -replace "<(.*?)([^\!\>])*>"
+
+        $cleaned = $nearlyClean -replace "[^{}]"
+
+        return $cleaned
+    }
+}
+
+Function RemoveNonGarbage {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$inputFile
+    )
+
+    Process {
+
+        $stringWithoutEscapedTags = $inputFile -replace "(!.)"
+
+        return $stringWithoutEscapedTags
+    }
+}
+
+Enum State {
+    Garbage
+    Clean
+}
+
+function CountGarbage {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$inputString
+    )
+
+    process {
+
+        $state = [State]::Clean
+
+        $numGarbage = 0
+
+        foreach($char in $inputString.GetEnumerator()) {
+            switch ($char) {
+                "<" {
+                    if($state -eq [State]::Clean){
+                        $state = [State]::Garbage
+                    }
+                    else {
+                        $numGarbage++
+                    }
+                }
+                ">" {
+                    if($state -eq [State]::Garbage){
+                        $state = [State]::Clean
+                    }
+                }
+                default {
+                    if($state -eq [State]::Garbage){
+                        $numGarbage++
+                    }
+                }
+            }
+        }
+
+        return $numGarbage
+    }
+}
+
+Function RemoveCharacters {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$stringWithDoubleChars,
+
+        [string]$Characters
+    )
+
+    Process {
+        return $stringWithDoubleChars.Replace($Characters, "")
+    }
+}
+
+Function ScoreString {
+    Param(
+        [parameter(ValueFromPipeline)]
+        [string]$inputString
+    )
+
+    Process {
+        
+        [int]$score = 0
+
+        [int]$layers = 0
+
+        foreach($char in $inputString.GetEnumerator()) {
+            switch ($char) {
+                "{" { 
+                    $layers++ 
+                    $score += $layers
+                }
+                "}" { 
+                    $layers--
+                }
+            }
+        }
+        return $score
+    }
+}
+
+
+$ans = ($file | CleanGarbage | ScoreString)
+
+write-host "First answer is: " $ans
+
+write-host -NoNewline "Second answer is: "
+$file | RemoveNonGarbage | CountGarbage | write-host 

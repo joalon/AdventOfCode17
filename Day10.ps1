99b75975a1a5319ecaf30d8e8df1527dfef58d19
root <joakimlonnegren@gmail.com>
2017-12-10 10:44:55 +0100

    Solved day 8 and problem 1, day 10

---
+++ Day10.ps1
@@ -0,0 +1,85 @@
+﻿$file = Get-Content .\input_day10.txt
+
+
+class Knot {
+
+    [int[]]$Circle
+
+    [int[]]$Lengths
+
+    [int]$CurrentPosition
+
+    [int]$Skip
+
+    [int]$CurrentLengthIndex
+
+    Knot([int[]]$circle, [int[]]$lengths, [int]$currentPosition, [int]$skip, [int]$currentLengthIndex) {
+        $this.Circle = $circle
+        $this.Lengths = $lengths
+        $this.CurrentPosition = $currentPosition
+        $this.Skip = $skip
+        $this.CurrentLengthIndex = $currentLengthIndex
+    }
+
+
+    [int[]] ReverseLength() {
+        $start = $this.Lengths[$this.CurrentLengthIndex]
+
+        [int[]]$pieceOfCircle = @()
+        while($start-- -gt 0) {
+            $pieceOfCircle += $this.Circle[$this.CurrentPosition]
+            if($this.CurrentPosition -eq ($this.Circle.Length-1)) {
+                $this.CurrentPosition = 0
+            }
+            else {
+                $this.CurrentPosition++
+            }
+        }
+
+
+        for($i = 0; $i -lt $pieceOfCircle.Length; $i++) {
+            if($this.CurrentPosition -eq 0) {
+                $this.CurrentPosition = ($this.Circle.Length-1)
+            }
+            else {
+                $this.CurrentPosition--
+            }
+            $this.Circle[$this.CurrentPosition] = $pieceOfCircle[$i]
+        }
+
+        $this.CurrentPosition += $this.Lengths[$this.CurrentLengthIndex]
+        $this.CurrentPosition += $this.Skip++
+
+        if($this.CurrentPosition -gt ($this.Circle.Length-1)) {
+            $this.CurrentPosition -= $this.Circle.Length
+        }
+
+        $this.CurrentLengthIndex++
+
+        return $this.Circle
+    }
+
+    [string] PrintSolution1() {
+        return "Index 0 and 1 multiplied is: " + ($this.Circle[0] * $this.Circle[1])
+    }
+}
+
+$file
+
+[int[]]$lengths = ($file.Split(',') | % { [int]$_ } )
+[int[]]$circle = 0..255
+
+
+
+
+#[int[]]$lengths = @(3,4,1,5)
+#[int[]]$circle = 0..4
+
+$knot = [Knot]::new($circle, $lengths, 0, 0, 0)
+
+$lengths | % { $knot.ReverseLength() }
+
+$knot.PrintSolution1()
+
+
+
--- Day8.ps1
+++ Day8.ps1
@@ -1,25 +1,84 @@
 ﻿$file = Get-Content .\input_day8.txt
 
-Enum Operator {
-    INC
-    DEC
-}
+$registers = @{}
 
-class KeyValuePair {
-    [string]$Key
-    [int]$Value
-}
+$file | % { 
+    $a = $_.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
 
-class Instruction {
-
-    [KeyValuePair]$register
-    [Operator]$Operator
-    [int]$Value
-
-
-    Instruction () {
-        
+    if($registers.ContainsKey($a[0]) -eq $false) {
+        $registers.Add($a[0], 0)
     }
-
 }
 
+$largestValueEver = -100000
+
+$file | % { 
+    $split = $_.Split(" ", [System.StringSplitOptions]::RemoveEmptyEntries)
+
+    switch($split[5]){
+        "<" {
+            if ($registers[$split[4]] -lt [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            }
+        }
+        ">" {
+            if ($registers[$split[4]] -gt [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            } 
+        }
+        ">=" { 
+            if ($registers[$split[4]] -ge [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            } 
+        }
+        "<=" { 
+            if ($registers[$split[4]] -le [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            }
+        }
+        "!=" { 
+            if ($registers[$split[4]] -ne [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            } 
+        }
+        "==" { 
+            if ($registers[$split[4]] -eq [int]$split[6]) {
+                switch($split[1]){
+                    "inc" { $registers[$split[0]] += [int]$split[2] }
+                    "dec" { $registers[$split[0]] -= [int]$split[2] }
+                }
+            }
+        }
+        default {
+            write-host "Didn't handle: " $split[5]
+        }
+    }
+    if($registers[$split[0]] -gt $largestValueEver) {
+            $largestValueEver = $registers[$split[0]]
+    }
+}
+
+$largestValueAtEnd = -10000
+$registers.Values | foreach {
+    if($_ -gt $largestValueAtEnd) {
+        $largestValueAtEnd = $_
+    }
+}
+
+write-host "Largest value at the end was: " $largestValue
+write-host "Largest value ever was: " $largestValueEver

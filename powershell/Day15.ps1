5ac8cb131f9bc0d21cbe137c4a6c3a293d850580
root <joakimlonnegren@gmail.com>
2017-12-19 21:06:16 +0100

    Brute-forces problem 1 day 15

---
+++ powershell/Day15.ps1
@@ -0,0 +1,32 @@
+﻿#$file = Get-Content ..\input\input_day15.txt
+
+class Generator {
+
+    [int]$Factor
+
+    [int]$CurrentNumber
+
+    hidden [int]$divisor = 2147483647
+
+    Generator([int]$factor, [int]$currentNumber) {
+        $this.Factor = $factor
+        $this.CurrentNumber = $currentNumber
+    }
+
+    [int] Generate() {
+        $this.CurrentNumber = $this.CurrentNumber * $this.Factor % $this.divisor
+
+        return $this.CurrentNumber
+    }
+
+    [int] GetCurrentNumber() {
+        return $this.CurrentNumber
+    }
+
+}
+
+#$genA = [Generator]::new(16807, 65)
+#$genB = [Generator]::new(48271, 8921)
+
+1..40000000 | % { $matches = 0; $genA = [Generator]::new(16807, 116); $genB = [Generator]::new(48271, 299) }{ $A = $genA.Generate(); $B = $genB.Generate(); if([uint16]($A -band 0xffff) -eq [uint16]($B -band 0xffff)) { $Matches++ } }{ return $Matches }
+
